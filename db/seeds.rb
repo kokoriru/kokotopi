1ce1c35006e056c302afc8a63ef35e5aa45ab160
kokoriru <kokorirury@yahoo.co.jp>
2016-02-07 01:08:37 +0900

    seed ファイルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Comment.delete_all
+Comment.connection.execute(%|SELECT setval ('comments_id_seq', 1, false)|)
+Topic.delete_all
+Topic.connection.execute(%|SELECT setval ('topics_id_seq', 1, false)|)
+Category.delete_all
+Category.connection.execute(%|SELECT setval ('categories_id_seq', 1, false)|)
+
+1.upto(3) do |i|
+  category = Category.create name: "category#{i}"
+
+  1.upto(3) do |j|
+    topic = Topic.create title: "title#{i}-#{j}", content: "content#{i}-#{j}", category: category
+
+    1.upto(3) do |k|
+      Comment.create author: "author#{i}-#{j}-#{k}", content: "content#{i}-#{j}-#{k}", topic: topic
+    end
+  end
+end
+
