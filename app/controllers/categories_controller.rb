9c7c7bfb5bf86ad9c96fde35e32f98920b821589
kokoriru <kokorirury@yahoo.co.jp>
2016-02-08 01:09:49 +0900

    categories の new ページを作成

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -7,4 +7,23 @@ class CategoriesController < ApplicationController
   def show
     @categories = Category.find(params[:id])
   end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to @category
+    else
+      render :new
+    end
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
