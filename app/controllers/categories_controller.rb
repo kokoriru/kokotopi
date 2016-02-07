89c0668141d3c34d8eecaac52385865296b3d54f
kokoriru <kokorirury@yahoo.co.jp>
2016-02-08 02:27:20 +0900

    categories ページ作成に伴う指摘事項を修正

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,11 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: %i[show edit update destroy]
 
   def index
     @categories = Category.all
   end
 
   def show
-    @category = Category.find(params[:id])
   end
 
   def new
@@ -23,11 +23,9 @@ class CategoriesController < ApplicationController
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
     if @category.update(category_params)
       redirect_to @category
     else
@@ -36,12 +34,17 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id]).destroy
+    @category.destroy
     redirect_to categories_path
   end
 
   private
-    def category_params
-      params.require(:category).permit(:name)
-    end
+
+  def set_category
+     @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
